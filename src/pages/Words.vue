719a308a81a462e7e3d7960f677a54489f4ba0e7
Pavel Gonzales <develop.gonzales@gmail.com>
2017-11-30 15:31:27 +0300

    add translater

--- src/pages/Words.vue
+++ src/pages/Words.vue
@@ -5,10 +5,13 @@
         v-list-tile(@click='' v-for="(item, i) in words", :key='i')
           v-list-tile-content 
             v-list-tile-title {{ item.word }}
-            v-list-tile-sub-title Перевод
+            v-list-tile-sub-title {{ item.translate || 'Перевода еще нет' }}
           v-list-tile-action
             v-btn(icon ripple @click="speak(item.word)")
               v-icon(light color='blue') volume_up
+          v-list-tile-action
+            v-btn(icon ripple @click="translate(item.word)")
+              v-icon(light) translate
           v-list-tile-action
             v-btn(icon ripple)
               v-icon(light) playlist_add
@@ -16,6 +19,10 @@
 </template>
 
 <script>
+  import axios from 'axios'
+  const API_URL = 'https://translate.yandex.net/api/v1.5/tr.json'
+  const API_KEY = 'trnsl.1.1.20171130T120759Z.8231e8a635e67fbb.fee57266a0cdfa56496c1aed4fcbf8a9d50f72a8'
+
   export default {
     data () {
       return {
@@ -31,6 +38,20 @@
     methods: {
       speak (word) {
         window.responsiveVoice.speak(word)
+      },
+      translate (word) {
+        axios
+          .get(`${API_URL}/translate?key=${API_KEY}&text=${word}&lang=en-ru&format=plain&options=1`)
+          .then((res) => {
+            console.log(res.data)
+            this.words = this.words.map((item) => {
+              if (word === item.word) {
+                item.translate = res.data.text[0]
+              }
+              return item
+            })
+            console.log(this.words)
+          })
       }
     }
   }
